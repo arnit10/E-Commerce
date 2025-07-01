d8226cdce978c8fb175ca6ad96c0bd5e766f51e6
arnit10 <arnitchauhan10@gmail.com>
2025-07-01 22:04:38 +0530

    added "Api is running" msg

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.use(cors({
   credentials: true,
 }));
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+
 app.use(express.json());
 
 app.use('/api/categories', categoryRoutes);
